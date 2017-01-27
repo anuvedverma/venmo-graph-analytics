2f097916432d1e103d8bf9630240f98a6a7044e9
Anuved Verma <anuvedverma@uchicago.edu>
2017-01-26 23:55:55 -0800

    testing redis-test for cluster

--- redis-test.py
+++ redis-test.py
@@ -1,5 +1,8 @@
 import redis
 
-r = redis.StrictRedis(host='localhost', port=6379, db=0)
+# REDIS_SERVER = 'ec2-52-33-8-227.us-west-2.compute.amazonaws.com'
+REDIS_SERVER = 'localhost'
+r = redis.StrictRedis(host=REDIS_SERVER, port=6379, db=0)
+# r = redis.StrictRedis(host='localhost', port=6379, db=0)
 r.set('foo', 'bar')
 print r.get('foo')
